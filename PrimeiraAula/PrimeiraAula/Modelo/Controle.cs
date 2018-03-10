46a88334833b8c366b7ee7aefa6c3f6468c09982
MatheusCruz23 <31697408+MatheusCruz23@users.noreply.github.com>
2018-03-10 20:02:42 -0300

    Add files via upload
    Começo do conceito de orientação a objeto

---
+++ PrimeiraAula/PrimeiraAula/Modelo/Controle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeiraAula.Modelo
+{
+    public class Controle
+    {
+        public String Calcular(String num1, String num2, String op)
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            String resposta = "";
+            try
+            {
+                n1 = Convert.ToDouble(num1);
+                n2 = Convert.ToDouble(num2);
+                if (op.Equals("+"))
+                    resultado = n1 + n2;
+                if (op.Equals("-"))
+                    resultado = n1 - n2;
+                if (op.Equals("*"))
+                    resultado = n1 * n2;
+                resposta = resultado.ToString("0.##");
+                if (op.Equals("/"))
+                    if (n2 == 0)
+                        resposta = "Divisão por zero";
+                    else
+                    {
+                        resultado = n1 / n2;
+                        resposta = resultado.ToString("0.##");
+                    }
+            }
+            catch (FormatException e)
+            {
+                resposta = "Digite um número válido";
+            }
+            return resposta;
+        }
+    }
+}
